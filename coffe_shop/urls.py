0f2e2a079b7322aefb2fe7029b36f095c6f9320f
Ernesto Gomez <erjgomezf@gmail.com>
2025-09-11 15:31:56 -0400

    Agregar la aplicación de usuarios: se implementan modelos, vistas, URLs y plantillas para gestionar la autenticación de usuarios.

--- coffe_shop/urls.py
+++ coffe_shop/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("productos/", include("products.urls")),  # Incluir las URLs de la app products
+    path("usuarios/", include("users.urls")),  # Incluir las URLs de la app users
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
